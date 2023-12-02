7cffcca7f3f9237583b53368ef4ce5d61cc44e6b
Xuan Thao <xuanthao@yopmail.com>
2023-12-02 22:24:04 +0700

    This is initial code

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use('/', express.static(__dirname + '/'))
+
+app.post('/save-task', function(req, response){
+    const taskObject = req.body
+    // connect database
+    // saves new task in database
+    console.log("saved task: ", taskObject.task)
+    response.send({savedTask: taskObject.task})
+})
+
+app.get('/get-tasks', function(req, jsonRes) {
+    const tasks = [
+        "Write JS code",
+        "Study TOEFL iBT",
+        "Hit the gymn"
+    ]
+    jsonRes.send({tasks: tasks})
+})
+
+
+app.listen(3000, function ()  {
+    console.log("app listening on port 3000")
+})
